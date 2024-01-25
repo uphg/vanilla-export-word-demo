311eaeb78b5395817ca58b08da3758ca71d387b1
lvheng <1834805770@qq.com>
2024-01-25 15:26:10 +0800

    feat: style tag

--- main.js
+++ main.js
@@ -22,7 +22,11 @@ btn.addEventListener('click', exportWord)
 
 function exportWord(){
   const header = `<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns="http://www.w3.org/TR/REC-html40">
-  <head><meta charset="utf-8"><title>Export HTML to Word Document with JavaScript</title></head><body>`
+  <head>
+    <meta charset="utf-8"><title>Export HTML to Word Document with JavaScript</title>
+    <style>body{ color: blue; }</style>
+  </head>
+  <body>`
   const footer = '</body></html>'
 
   const sourceHTML = header+document.getElementById("source-html").innerHTML+footer;
